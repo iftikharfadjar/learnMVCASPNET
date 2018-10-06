89242749367ed6c5db34e466ecf15103c4407b05
FadjarIftikhar <fadjar.iftikhar@gmail.com>
2018-10-06 13:53:29 +0700

    DEMO MVC ASP NET
    This is DEMO of MVC in ASP NET

---
+++ Exercise2/Exercise2/Exercise2/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Exercise2.Models;
+
+namespace Exercise2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { id = 1, Name = "Wiro Sableng"},
+                new Movie { id = 2, Name = "Petani Sukses"}
+            };
+        }
+    }
+}
